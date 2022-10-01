7d33586330f0a8f445bced24ab3eccc9094f5595
Denis <denisguedesmoraes@gmail.com>
2022-10-01 20:14:03 -0300

    recebendo os valores do input, e somando no total okay, falta fazer a logica do input radius

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,39 @@
 /* Desenvolva sua lógica aqui */
+import { insertedValues, valuesCategory } from "../../pages/home/valuesData.js";
+
+const identificarOIndex = valuesCategory;
+console.log(identificarOIndex);
+const valores = insertedValues;
+console.log(valores);
+
+export function renderizarAll() {
+  valores.forEach((element) => {
+    const main = document.querySelector(".conteiner");
+    const SomaTotal = document.createElement("p");
+    const ul = document.createElement("ul");
+
+    const li = document.createElement("li");
+    li.setAttribute("class", "valores");
+    const valores = document.createElement("p");
+    const spanbtn = document.createElement("span");
+    const excluir = document.createElement("button");
+    const button = document.createElement("button");
+
+    spanbtn.setAttribute("class", "btnSpan");
+    button.setAttribute("class", "tipoDeMovimentacao");
+    excluir.setAttribute("class", "lixeira");
+    valores.innerText = `R$ ${element.value}`;
+    if (element.categoryID === 1) {
+      button.innerText = "Entrada";
+    } else {
+      button.innerText = "saida";
+    }
+    excluir.innerText = "remover";
+
+    spanbtn.append(button, excluir);
+    li.append(SomaTotal, valores, spanbtn);
+    ul.append(li);
+    main.appendChild(ul);
+  });
+}
+renderizarAll();
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,184 @@
 /* Desenvolva sua lógica aqui */
+import { renderizarAll } from "../pages/home/index.js";
+import { insertedValues, valuesCategory } from "../pages/home/valuesData.js";
+const renderAll = renderizarAll;
+const data = insertedValues;
+const category = valuesCategory;
+console.log(data);
+
+const btnRegistrer = document.getElementById("Registrar");
+
+btnRegistrer.addEventListener("click", () => {
+  openModal();
+});
+
+function openModal(event) {
+  // event.preventDefault();
+  const section = document.querySelector(".modal");
+
+  const modal = document.querySelector(".modal");
+  modal.classList.toggle("show_modal");
+  console.log("heloow world");
+
+  const bodyConteiner = document.querySelector("body");
+
+  const divConteiner = document.createElement("div");
+  const spanBtn = document.createElement("span");
+  spanBtn.setAttribute("class", "spanBtn");
+
+  divConteiner.setAttribute("class", "conteinerModal");
+
+  const divModalBox = document.createElement("div");
+  divModalBox.setAttribute("class", "modal_box");
+
+  const divModalHeader = document.createElement("div");
+  divModalHeader.setAttribute("class", "modal_header");
+  const title = document.createElement("p");
+  const text = document.createElement("p");
+  const buttonClose = document.createElement("button");
+
+  title.setAttribute("class", "tituloModal");
+  buttonClose.setAttribute("class", "close_modal");
+  buttonClose.innerText = "X";
+  buttonClose.addEventListener("click", () => {
+    closeModal(modal);
+  });
+
+  const butonREg = document.createElement("button");
+  butonREg.setAttribute("class", "btn-register");
+  butonREg.innerText = "Registrar";
+  // buttonregistrar
+  butonREg.addEventListener("click", () => {
+    novoValor();
+    const inputValue = document.getElementById("inputValue");
+    const entrada = document.getElementById("1");
+    const saida = document.getElementById("2");
+
+    somaValores(insertedValues);
+
+    // renderAll();
+  });
+
+  const button = document.createElement("input");
+  button.setAttribute("class", "btnEntrada");
+  button.setAttribute("type", "radio");
+  button.setAttribute("id", "1");
+  button.setAttribute("value", "0");
+  button.setAttribute("name", "valueType");
+  const label = document.createElement("label");
+  label.setAttribute("class", "labelBt");
+  label.setAttribute("tabindex", "0");
+  label.setAttribute("for", "0");
+  label.innerText = "Entrada";
+  const buttoSaida = document.createElement("input");
+
+  buttoSaida.setAttribute("type", "radio");
+  buttoSaida.setAttribute("id", "2");
+  buttoSaida.setAttribute("value", "0");
+  buttoSaida.setAttribute("name", "valueType");
+  const labelSaida = document.createElement("label");
+  labelSaida.setAttribute("class", "labelBt");
+  labelSaida.setAttribute("tabindex", "0");
+  labelSaida.setAttribute("for", "0");
+  labelSaida.innerText = "Saida";
+
+  const spanButton = document.createElement("span");
+  spanButton.setAttribute("class", "btnsContent");
+  const buttonCancelar = document.createElement("button");
+
+  buttonCancelar.innerText = "Cancelar";
+  buttonCancelar.setAttribute("class", "btnCancel");
+
+  button.innerText = "Entrada";
+  buttoSaida.innerText = "Saida";
+
+  {
+    /* <input type="radio" hidden id="0" value="0" name="valueType">
+<label class="bt bt-outline" tabindex="0" for="0">Entrada</label> */
+  }
+  button.setAttribute("class", "btnEntrada");
+  buttoSaida.setAttribute("class", "btnSaida");
+  button.setAttribute("id", "1");
+  buttoSaida.setAttribute("id", "2");
+
+  const input = document.createElement("input");
+
+  input.setAttribute("class", "inputValor");
+  input.setAttribute("id", "inputValue");
+  title.innerText = "Registro de valor";
+  text.innerText =
+    "digite o valor e em seguida aperte no botão referente ao tipo do valor";
+  input.placeholder = "R$00,00";
+
+  input.setAttribute("type", "number");
+
+  spanButton.append(buttonCancelar, butonREg);
+  spanBtn.append(button, label, buttoSaida, labelSaida);
+  divConteiner.append(buttonClose, title, text, input, spanBtn, spanButton);
+  divModalBox.appendChild(divConteiner);
+
+  section.append(divModalBox);
+}
+
+function closeModal(modal) {
+  console.log("hola mundo");
+  modal.classList.toggle("show_modal");
+  modal.innerHTML = "";
+}
+
+function novoValor(data) {
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  li.setAttribute("class", "valores");
+  const input = document.getElementById("inputValue");
+  const valor = document.createElement("p");
+  const value = document.createElement("p");
+  const spanbutton = document.createElement("span");
+  const buttonName = document.createElement("button");
+  const buttonexcluir = document.createElement("button");
+  const main = document.querySelector(".conteiner");
+
+  spanbutton.setAttribute("class", "SpanButon");
+  buttonexcluir.setAttribute("class", "lixeira");
+  buttonName.setAttribute("class", "btnName");
+
+  //inserir valores no meu arr
+  buttonexcluir.innerText = "excluir";
+  buttonName.innerText = "Entrada";
+  valor.innerText = `R$ ${input.value}`;
+
+  li.append(value, valor, spanbutton);
+  ul.appendChild(li);
+  main.appendChild(ul);
+
+  spanbutton.append(buttonName, buttonexcluir);
+
+  insertedValues.push({
+    value: input.value,
+    categoryID: 1,
+  });
+
+  const newArr = insertedValues.map((elem, index) => {
+    return {
+      categoryId: elem,
+      id: index,
+    };
+  });
+
+  console.log(newArr);
+}
+
+function somaValores(data) {
+  const total = document.querySelector(".valorTotal");
+  total.innerHTML = "";
+  let totally = [];
+  data.forEach((valor) => {
+    totally.push(valor.value);
+  });
+
+  total.innerText = `R$ ${totally.reduce(
+    (valor, result) => parseInt(valor) + parseInt(result),
+    0
+  )}`;
+}
+somaValores(data);
